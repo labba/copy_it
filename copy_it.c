462c8a80299a6de168cb822c95315fe3a206d6a1
labba <labbala@gmail.com>
2020-01-29 10:52:20 +0200

    Add files via upload

---
+++ copy_it.c
@@ -0,0 +1,55 @@
+#define BUFFERSIZE 25
+#define TRUE 1
+#define FALSE 0
+
+int copy_it(char *input)
+{
+	char lbuf[BUFFERSIZE];
+	char c, *p = input, *d = &lbuf[0];
+	char *ulimit = &lbuf[BUFFERSIZE-10];
+	int quotation = FALSE;
+	int rquote = FALSE;
+
+	memset(lbuf, 0, BUFFERSIZE);
+	
+	while((c = *p++) != '\0')
+	{
+		if ((c == '<') && (!quotation))
+		{
+			quotation = TRUE;
+			ulimit--;
+		}
+		if ((c == '>') && (quotation))
+		{
+			quotation = FALSE;
+			ulimit++;
+		}
+		if (c == '(' && !quotation && !rquote)
+		{
+			rquote = TRUE;
+			// FIX: insert ulimit--; here
+		}
+		if (c == ')' && !quotation && rquote)
+		{
+			rquote = FALSE;
+			ulimit++;
+		}
+		if (d < ulimit)
+			*d++ = c;
+	}
+	if (rquote)
+		*d++ = ')';
+	if (quotation)
+		*d++ = '>';
+    
+    return 0;
+}
+	
+
+int main(int argc, char *argv[])
+{
+	if (argc != 2)
+		return -1;
+
+	return copy_it(argv[1]);
+}
